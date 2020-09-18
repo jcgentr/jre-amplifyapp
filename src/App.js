d7cffe564537a185b3e498017d2e8e1cf3ba97c3
jcgentr <jcgentr@clemson.edu>
2020-09-18 16:32:15 -0400

    add styling and add Sidebar and TranscriptPage components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Sidebar from './components/Sidebar'
+import TranscriptPage from './components/TranscriptPage'
+
 function App() {
   return (
     <div className="App">
-      <div className="sidebar">
-        <button className="epLink">#1536 - Edward Snowden</button>
-        <button className="epLink">#1536 - Edward Snowden</button>
-        <button className="epLink">#1536 - Edward Snowden</button>
-        <button className="epLink">#1536 - Edward Snowden</button>
-        <button className="epLink">#1536 - Edward Snowden</button>
-        <button className="epLink">#1536 - Edward Snowden</button>
-        <button className="epLink">#1536 - Edward Snowden</button>
-        <button className="epLink">#1536 - Edward Snowden</button>
-      </div>
-      <div className="transcriptContainer">
-        <iframe 
-          title="p1536"
-          width="560" 
-          height="315" 
-          src="https://www.youtube.com/embed/_Rl82OQDoOc" 
-          frameborder="0" 
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-          allowfullscreen
-        ></iframe>
-      </div>
+      <Sidebar />
+      <TranscriptPage />
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <button className="epLink">#1536 - Edward Snowden</button>
+      <button className="epLink">#1536 - Edward Snowden</button>
+      <button className="epLink">#1536 - Edward Snowden</button>
+      <button className="epLink">#1536 - Edward Snowden</button>
+      <button className="epLink">#1536 - Edward Snowden</button>
+      <button className="epLink">#1536 - Edward Snowden</button>
+      <button className="epLink">#1536 - Edward Snowden</button>
+      <button className="epLink">#1536 - Edward Snowden</button>
+    </div>
+  )
+}
+
+export default Sidebar
